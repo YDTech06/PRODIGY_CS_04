fcdc1cb86b3e145983b32da2f5e727001e75ddd5
YDTech06 <yuvarajdhanavade06@gmail.com>
2024-08-30 20:10:38 +0530

    Task-04-Key_Logger_Tool.py

--- Task-04-Key_Logger_Tool.py
+++ Task-04-Key_Logger_Tool.py
@@ -12,6 +12,7 @@ from pynput import keyboard
 class KeyLoggerApp(QMainWindow):
     # INITIALIZATION
     def __init__(self):
+        
         # Initialize the KeyLogger application.
         super().__init__()
         self.initUI()
